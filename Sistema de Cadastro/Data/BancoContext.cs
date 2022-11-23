93b2ce27c7eec0cf28c4a3c28d545e654eaa7130
Mikaelle <101604823+mikaelleps@users.noreply.github.com>
2022-11-23 18:32:29 -0300

    Confirmar apagar contato

---
+++ Sistema de Cadastro/Data/BancoContext.cs
@@ -0,0 +1,7 @@
+﻿namespace Sistema_de_Cadastro.Data
+{
+    public class BancoContext
+    {
+  
+    }
+}
---
+++ Sistema de Cadastro/Models/ContatoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Sistema_de_Cadastro.Models
+{
+    public class ContatoModel
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+    }
+}
