fd5b6a56032dff49605a171ba2be41cc766d421a
Mikaelle <101604823+mikaelleps@users.noreply.github.com>
2022-11-22 22:36:59 -0300

    criar, editar, apagar

--- Sistema de Cadastro/Controllers/HomeController.cs
+++ Sistema de Cadastro/Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace Sistema_de_Cadastro.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
